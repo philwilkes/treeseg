9afa522180edaa1934b1fb9a40c467769e44918d
philwilkes <p.wilkes@ucl.ac.uk>
2018-05-17 22:07:05 +0100

    retab

--- src/tile_index.cpp
+++ src/tile_index.cpp
@@ -14,25 +14,25 @@
 
 int main (int argc, char** argv)
 {
-	float area = atof(argv[1]);
-	float half_length = sqrt(area)/2;
-	for(int i=2;i<argc;i++)
-	{
+    float area = atof(argv[1]);
+    float half_length = sqrt(area)/2;
+    for(int i=2;i<argc;i++)
+    {
         boost::filesystem::path oname(argv[i]);
-		std::vector<std::string> fname1, fname2;
+        std::vector<std::string> fname1, fname2;
         boost::split(fname1, oname.stem().string(), boost::is_any_of("."));
         boost::split(fname2, fname1.at(0), boost::is_any_of("_"));
-		std::string name = fname2.at(1);
+        std::string name = fname2.at(1);
 
-		pcl::PCDReader reader;
-		pcl::PCDWriter writer;
-		pcl::PointCloud<pcl::PointXYZ>::Ptr plot(new pcl::PointCloud<pcl::PointXYZ>);
-		reader.read(argv[i],*plot);
+        pcl::PCDReader reader;
+        pcl::PCDWriter writer;
+        pcl::PointCloud<pcl::PointXYZ>::Ptr plot(new pcl::PointCloud<pcl::PointXYZ>);
+        reader.read(argv[i],*plot);
 
-		Eigen::Vector4f min,max,centroid;
-		pcl::compute3DCentroid(*plot,centroid);
-		pcl::getMinMax3D(*plot,min,max);
-		std::cout << name << " " << min[0]+half_length <<" " << min[1]+half_length << std::endl;
-	}
+        Eigen::Vector4f min,max,centroid;
+        pcl::compute3DCentroid(*plot,centroid);
+        pcl::getMinMax3D(*plot,min,max);
+        std::cout << name << " " << min[0]+half_length <<" " << min[1]+half_length << std::endl;
+    }
     return 0;
 }
